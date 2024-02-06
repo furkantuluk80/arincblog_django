9966221d71e18bb48fb77ccc6f4e67d8ff509b2c
furkantuluk80 <furkan.tlk80@gmail.com>
2024-02-06 20:30:53 +0300

    v0.0.4 - projenin ayaga kaldirilmasi
    ##############################################
    Ad Soyad : Furkan Tuluk
    Tarih    : 06/02/2024
    Versiyon : v0.0.4
    Aciklama :
    -proje ayaga kaldirilmistir.
    -veri tabani olusturuldu.
    -html kod dosyalari duzenlenmistir.
    -settings.py ve urls.py dosyalari
     duzenlenmistir
    
    ##############################################

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from views import HomePageView
+from .views import HomePageView
 
 """
 Burada amac, siteye giriste herhangi bir adrese gidilmemisse('')
-ilk gelen sayfadaki gorunumu belirlemek. Urls dosyasi ile 
+ilk gelen sayfadaki(anasayfa) gorunumu belirlemek. Urls dosyasi ile 
 views dosyasindaki HomePageView sinifi referans alinarak
 class'in islenmesi saglanir.
 """
