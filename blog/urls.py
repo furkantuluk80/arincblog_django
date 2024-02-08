9e186920a042cb3131c65422bcb83e49cc8c9f2c
furkantuluk80 <furkan.tlk80@gmail.com>
2024-02-08 10:59:15 +0300

    v0.0.7 - tekil post sayfasi
    ##############################################
    Ad Soyad : Furkan Tuluk
    Tarih    : 08/02/2024
    Versiyon : v0.0.7
    Aciklama :
    - primary key yapisi ile post sayfasindan baska
      sayfalara yonledirilme saglandi.
    - post.html dosyasinda unutulan Page Header sayfasi
      eklendi.
    ##############################################

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import BlogPageView
+from .views import BlogPageView, BlogDetailView
 
 """
 Burada amac, siteye giriste herhangi bir adrese gidilmemisse('')
@@ -7,7 +7,14 @@ ilk gelen sayfadaki(anasayfa) gorunumu belirlemek. Urls dosyasi ile
 views dosyasindaki HomePageView sinifi referans alinarak
 class'in islenmesi saglanir.
 """
+"""
+'post/<int:pk>/': Bu desen, URL'nin yapisini tanimlar. 
+post/ ile başlar ve sonrasinda bir <int:pk> kismi gelir. <int:pk> kismi, birincil anahtarin (primary key) 
+bir tamsayi (integer) olduğunu belirtir. Bu, URL'nin bu bolumune bir tamsayi degeri bekledigimizi ifade eder.
+"""
 urlpatterns = [
     path('',BlogPageView.as_view(), name = 'index'  ),
+
+    path('post/<int:pk>/',BlogDetailView.as_view(), name = 'post'  ), 
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 """
@@ -10,6 +10,14 @@ class BlogPageView(ListView):
     model = Post     #model olarak post kullanilir demek.
     template_name = 'index.html'
     
-    
+
+"""
+Bu  sinif, localhost ilk açıldığı anda
+gelen linklere tiklandiginda, o linkin
+ilgili sayfasina yonledirilmesini saglar.
+"""  
+class BlogDetailView(DetailView):    
+    model = Post     #model olarak post kullanilir demek.
+    template_name = 'post.html'
     
 
